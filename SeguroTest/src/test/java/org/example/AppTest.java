18dd1475074bb28c7f611da8982444e4840e1a50
@jmendez08 <mendez8079@gmail.com>
2021-10-26 23:17:44 -0500

    Cambios en apptest

--- SeguroTest/src/test/java/org/example/AppTest.java
+++ SeguroTest/src/test/java/org/example/AppTest.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +16,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AppTest 
 {
     private WebDriver driver;
+    By edadLocalizador=By.name("edad");
+    By generoLocalizador=By.name("genero");
+    By btn_consultLocalizador=By.name("btn_conusltar");
+    By bodySelector =By.cssSelector("body");
 
     @Before
     public void setup(){
@@ -23,9 +29,13 @@ public class AppTest
         driver.get("http://localhost");
     }
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.findElement(generoLocalizador).sendKeys("masculino");
+        driver.findElement(edadLocalizador).sendKeys("18");
+        driver.findElement(btn_consultLocalizador).click();
+        assertEquals("El valor de la prima anual de seguro de auto según su género y edad es: 2000 dólares",driver.findElement(bodySelector).getText());
+        assertTrue( true);
     }
 
     @After
